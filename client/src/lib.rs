a914c81f136d65aa1d062592e4301dce4a845353
Cdita <seftraiz@gmail.com>
2026-05-28 13:10:17 +0300

    Update lib.rs

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -4,11 +4,11 @@ use std::ffi::c_void;
 use std::thread;
 use std::time::Duration;
 use winapi::um::winnt::DLL_PROCESS_ATTACH;
-use winapi::um::winuser::*;
+use winapi::um::winuser::GetAsyncKeyState;
 
 mod webview_chat;
 
-use webview_chat::WEBVIEW_CHAT;
+use webview_chat::{init_webview, add_message, show_chat, hide_chat, is_chat_visible};
 
 #[no_mangle]
 pub extern "system" fn DllMain(_hinst: *mut c_void, reason: u32, _reserved: *mut c_void) -> u32 {
@@ -17,16 +17,17 @@ pub extern "system" fn DllMain(_hinst: *mut c_void, reason: u32, _reserved: *mut
             thread::spawn(|| {
                 thread::sleep(Duration::from_secs(3));
                 
-                let mut chat = WEBVIEW_CHAT.lock().unwrap();
-                chat.init();
-                chat.add_message("RebornMP Loaded! Press T to chat", true);
+                init_webview();
+                add_message("RebornMP Loaded! Press T to chat", true);
                 
                 loop {
                     unsafe {
-                        // Проверяем клавишу T
-                        if GetAsyncKeyState(0x54) & 1 != 0 {
-                            let mut chat = WEBVIEW_CHAT.lock().unwrap();
-                            chat.show_chat();
+                        if GetAsyncKeyState(0x54) & 1 != 0 { // VK_T
+                            if is_chat_visible() {
+                                hide_chat();
+                            } else {
+                                show_chat();
+                            }
                         }
                     }
                     thread::sleep(Duration::from_millis(50));
