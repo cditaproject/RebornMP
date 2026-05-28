331ba0af232a99095d4eee6fce853bada249510d
Cdita <seftraiz@gmail.com>
2026-05-28 11:38:52 +0300

    Update memory.rs

--- client/src/memory.rs
+++ client/src/memory.rs
@@ -1,3 +1,63 @@
-pub fn init() {
-    println!("[Memory] Initialized");
+// client/src/memory.rs
+// RebornMP Memory Manager - GTA V Memory Operations
+
+// Оффсеты для GTA V (нужно уточнить версию)
+const OFFSET_PLAYER_POS_X: usize = 0x00;
+const OFFSET_PLAYER_POS_Y: usize = 0x04;
+const OFFSET_PLAYER_POS_Z: usize = 0x08;
+const OFFSET_PLAYER_HEALTH: usize = 0x280;
+const OFFSET_PLAYER_ARMOUR: usize = 0x284;
+const OFFSET_PLAYER_WANTED: usize = 0x2A0;
+
+/// Получить позицию игрока из памяти
+pub fn get_player_position() -> Option<(f32, f32, f32)> {
+    // В реальной реализации:
+    // 1. Найти адрес CPlayerInfo
+    // 2. Прочитать координаты
+    // Пока возвращаем тестовые значения
+    unsafe {
+        // TODO: Реализовать чтение из памяти GTA V
+        // Для теста возвращаем нулевую позицию
+        Some((0.0, 0.0, 0.0))
+    }
+}
+
+/// Получить здоровье игрока
+pub fn get_player_health() -> u32 {
+    unsafe {
+        // TODO: Реализовать чтение здоровья
+        100
+    }
+}
+
+/// Получить броню игрока
+pub fn get_player_armour() -> u32 {
+    unsafe {
+        // TODO: Реализовать чтение брони
+        0
+    }
+}
+
+/// Получить уровень розыска
+pub fn get_wanted_level() -> u32 {
+    unsafe {
+        // TODO: Реализовать чтение уровня розыска
+        0
+    }
+}
+
+/// Установить позицию игрока
+pub fn set_player_position(x: f32, y: f32, z: f32) -> bool {
+    unsafe {
+        // TODO: Реализовать запись в память
+        true
+    }
+}
+
+/// Установить здоровье игрока
+pub fn set_player_health(health: u32) -> bool {
+    unsafe {
+        // TODO: Реализовать запись здоровья
+        true
+    }
 }
